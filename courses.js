7dbd2617c6c6ff58f3458501ef874d2a216b4153
fsoria <fersoria02@gmail.com>
2023-11-13 07:12:53 -0300

    Modality courses

--- courses.js
+++ courses.js
@@ -1,14 +1,15 @@
-function updateCourseDetails() {
-    var modalidadSelect = document.getElementById("modalidadSelect");
-    var precioCurso = document.getElementById("precioCurso");
+function updateCourseDetails(courseNumber) {
+    var modalidadSelect = document.getElementById("modalidadSelect" + courseNumber);
+    var precioCurso = document.getElementById("precioCurso" + courseNumber);
+    var estudiantesCurso = document.getElementById("estudiantesCurso" + courseNumber);
 
     var selectedModalidad = modalidadSelect.value;
 
     if (selectedModalidad === "semipresencial") {
-        precioCurso.innerText = "$17.000";
-        // Puedes actualizar otros detalles aquí.
-    } else if (selectedModalidad === "personalizado") {
         precioCurso.innerText = "$30.000";
-        // Puedes actualizar otros detalles aquí.
+        estudiantesCurso.innerText = "4 estudiantes";
+    } else if (selectedModalidad === "personalizado") {
+        precioCurso.innerText = "$45.000";
+        estudiantesCurso.innerText = "1 estudiante";
     }
 }
