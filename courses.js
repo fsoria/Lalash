acb5e9f9798d7900858bfa7a5a9a511b37337959
fsoria <fersoria02@gmail.com>
2023-11-22 21:44:32 -0300

    Add link drive info courses

--- courses.js
+++ courses.js
@@ -5,11 +5,46 @@ function updateCourseDetails(courseNumber) {
 
     var selectedModalidad = modalidadSelect.value;
 
-    if (selectedModalidad === "semipresencial") {
+    if (courseNumber === 1) {
+        if (selectedModalidad === "Personalizado") {
+            precioCurso.innerText = "$60.000";
+            estudiantesCurso.innerText = "1 estudiante";
+        } else if (selectedModalidad === "Grupal") {
+            precioCurso.innerText = "$45.000";
+            estudiantesCurso.innerText = "5 estudiantes";
+        }
+        else if (selectedModalidad === "100% Online") {
         precioCurso.innerText = "$30.000";
-        estudiantesCurso.innerText = "4 estudiantes";
-    } else if (selectedModalidad === "personalizado") {
-        precioCurso.innerText = "$45.000";
-        estudiantesCurso.innerText = "1 estudiante";
+        estudiantesCurso.innerText = "30 estudiantes";
+        }
+        
+    } else if (courseNumber === 2) {
+        if (selectedModalidad === "100% Online") {
+            precioCurso.innerText = "$15.000";
+            estudiantesCurso.innerText = "30 estudiantes"
+        }
+    }
+    else if (courseNumber === 3) {
+        if (selectedModalidad === "Presencial") {
+            precioCurso.innerText = "$40.000";
+            estudiantesCurso.innerText = "3 estudiantes";
+        } else if (selectedModalidad === "Semipresencial") {
+            precioCurso.innerText = "$32.000";
+            estudiantesCurso.innerText = "3 estudiantes";
+        }
+        else if (selectedModalidad === "100% Online") {
+        precioCurso.innerText = "$26.000";
+        estudiantesCurso.innerText = "30 estudiantes";
+        }
+    }
+    else if (courseNumber === 4) {
+        if (selectedModalidad === "Grupal") {
+            precioCurso.innerText = "$17.000";
+            estudiantesCurso.innerText = "5 estudiantes"
+        }
+        else if (selectedModalidad === "Personalizado") {
+            precioCurso.innerText = "$30.000";
+            estudiantesCurso.innerText = "3 estudiantes";
+        }
     }
 }
